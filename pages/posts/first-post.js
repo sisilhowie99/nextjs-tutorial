183a1cde9fca717177adb956aad0c42084de070e
sisilhowie99 <sisil.anastasia@gmail.com>
2022-08-27 23:05:22 +1000

    Add image and title
    * Download image to public directory
    * Use <Image /> to display image in first-post page
    * Use <Head /> to add metadata title for first-post page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,19 @@
+import Head from 'next/head';
+import Image from 'next/image';
 import Link from "next/link";
+
+const ProfilePic = () => (
+    <Image src='/images/profile.jpg' height={144} width={144} alt='Profile' />
+)
 
 export default function FirstPost() {
 	return (
 		<>
+            <Head>
+                <title>First post</title>
+            </Head>
 			<h1>First Post</h1>
+            <ProfilePic />
 			<h2>
 				<Link href="/">Return home</Link>
 			</h2>
