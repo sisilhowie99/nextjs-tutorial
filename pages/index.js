ccf2cd61f5979041c22bdcff924546b5cc655e28
sisilhowie99 <sisil.anastasia@gmail.com>
2022-09-02 00:46:40 +1000

    Implement getStaticProps. Change line endings to LF

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,42 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello, my name is Sisilia! I'm a Software Engineer. I'm now learning Next.js</p>
-        <p>
-          This is a sample website I made. You'll be building a site like this on <a href='https://nextjs.org/learn'>Next.js tutorial</a>.
-        </p>
-      </section>
-    </Layout>
-  )
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: { allPostsData },
+	};
+}
+
+export default function Home({ allPostsData }) {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>Hello, my name is Sisilia! I'm a Software Engineer. I'm now learning Next.js</p>
+				<p>
+					This is a sample website I made. You'll be building a site like this on{" "}
+					<a href="https://nextjs.org/learn">Next.js tutorial</a>.
+				</p>
+			</section>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,18 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import Layout from '../../components/layout';
+import Layout from "../../components/layout";
 
-const ProfilePic = () => (
-    <Image src='/images/profile.jpg' height={144} width={144} alt='Profile' />
-)
+const ProfilePic = () => <Image src="/images/profile.jpg" height={144} width={144} alt="Profile" />;
 
 export default function FirstPost() {
 	return (
 		<Layout>
-            <Head>
-                <title>First post</title>
-            </Head>
+			<Head>
+				<title>First post</title>
+			</Head>
 			<h1>First Post</h1>
-            <ProfilePic />
+			<ProfilePic />
 			<h2>
 				<Link href="/">Return home</Link>
 			</h2>
